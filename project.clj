3033124a9396803043ef507e30edfc1829d0a368
kawasima <kawasima1016@gmail.com>
2015-02-17 21:39:52 +0900

    Changed to use fixed version dependencies.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [javax/javaee-api "7.0"]
                  [environ "1.0.0"]
                  [net.unit8.wscl/websocket-classloader "0.2.0"]
-                 [net.unit8.logback/logback-websocket-appender "0.1.0-SNAPSHOT"]
+                 [net.unit8.logback/logback-websocket-appender "0.1.0"]
                  [io.undertow/undertow-websockets-jsr "1.1.1.Final"]
                  [com.datomic/datomic-free "0.9.5130" :exclusions [org.slf4j/slf4j-api org.slf4j/slf4j-nop] ]
                  [datomic-schema "1.2.1"]
