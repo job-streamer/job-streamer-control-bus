51d01854bc75c3abfdd84bb4db8eba85be1938f4
Kazuki Tsutsumi <rixi223.kazuki@gmail.com>
2016-12-16 17:20:28 +0900

    Added the test-cases for auth-component.

--- src/clj/job_streamer/control_bus/component/auth.clj
+++ src/clj/job_streamer/control_bus/component/auth.clj
@@ -13,7 +13,7 @@
             (job-streamer.control-bus.component [datomic :as d]
                                                 [token :as token])))
 
-(defn auth-by-password [datomic user-id password app-name]
+(defn- auth-by-password [datomic user-id password app-name]
   (when (and (not-empty user-id) (not-empty password))
     (when-let [user (d/query datomic
                              '{:find [(pull ?s [:*]) .]
@@ -52,67 +52,67 @@
                    :where [[?u :user/id ?id]]}
                  id)))
 
-(defn- validate-user [datomic user]
-  (b/validate user
-              :user/password [[v/required :pre (comp nil? :user/token)]
-                              [v/min-count 8 :message "Password must be at least 8 characters long." :pre (comp nil? :user/token)]]
-              :user/token    [[v/required :pre (comp nil? :user/password)]
-                              [v/matches #"[0-9a-z]{16}" :pre (comp nil? :user/password)]]
-              :user/id       [[v/required]
-                              [v/min-count 3 :message "Username must be at least 3 characters long."]
-                              [v/max-count 20 :message "Username is too long."]
-                              [unique-name-validator datomic]]))
+(defvalidator exist-roll-validator
+  {:default-message-format "%s is used by someone."}
+  [roll-name datomic]
+  (d/query datomic
+           '[:find ?e .
+             :in $ ?n
+             :where [?e :roll/name ?n]]
+           roll-name))
 
-(defn- signup-to-default [datomic user roll-name]
-  (when-let [user (let [[result map] (validate-user datomic user)]
-                    (if-let [error-map (:bouncer.core/errors map)]
-                      (log/error error-map) ;; (signup-view {:error-map error-map :params user})
-                      (let [salt (nonce/random-nonce 16)
-                            password (some-> (not-empty (:user/password user))
-                                             (.getBytes)
-                                             (#(into-array Byte/TYPE (concat salt %)))
-                                             buddy.core.hash/sha256
-                                             buddy.core.codecs/bytes->hex)]
-                        (if-not (or password (:user/token user))
-                          (throw (Exception.)))
-                        (let [user (merge user
-                                          {:db/id #db/id[db.part/user -1]}
-                                          (when password
-                                            {:user/password password
-                                             :user/salt salt})
-                                          (when-let [token (:user/token user)]
-                                            {:user/token token}))]
-                          (d/transact datomic [user])
-                          user))))]
-    (when-let [roll-id (d/query datomic
-                                '[:find ?e .
-                                  :in $ ?n
-                                  :where [?e :roll/name ?n]]
-                                roll-name)]
-      (let [member-id (d/tempid :db.part/user)
-            app (d/query datomic
-                         '[:find (pull ?e [*]) .
-                           :in $ ?n
-                           :where [?e :application/name ?n]]
-                         "default")]
-        (d/transact datomic [{:db/id member-id
-                              :member/user [:user/id (:user/id user)]
-                              :member/rolls [roll-id]}
-                             {:db/id (:db/id app)
-                              :application/members (conj (:application/members app) member-id)}])))))
+(defn- signup [datomic user roll-name]
+  (when-let [user (let [salt (nonce/random-nonce 16)
+                        password (some-> (not-empty (:user/password user))
+                                         (.getBytes)
+                                         (#(into-array Byte/TYPE (concat salt %)))
+                                         buddy.core.hash/sha256
+                                         buddy.core.codecs/bytes->hex)]
+                    (if-not (or password (:user/token user))
+                      (throw (Exception.))
+                      (merge user
+                             {:db/id #db/id[db.part/user -1]}
+                             (when password
+                               {:user/password password
+                                :user/salt salt})
+                             (when-let [token (:user/token user)]
+                               {:user/token token}))))]
+    (let [roll-id (d/query datomic
+                           '[:find ?e .
+                             :in $ ?n
+                             :where [?e :roll/name ?n]]
+                           roll-name)
+          member-id (d/tempid :db.part/user)
+          app (d/query datomic
+                       '[:find (pull ?e [*]) .
+                         :in $ ?n
+                         :where [?e :application/name ?n]]
+                       "default")]
+      (d/transact datomic [user
+                           {:db/id member-id
+                            :member/user (select-keys user [:db/id])
+                            :member/rolls [roll-id]}
+                           (update-in (select-keys app [:db/id :application/members]) [:application/members] #(conj % member-id))]))))
 
-(defn login [{:keys [datomic token] :as component} {{:keys [username password appname next back]} :params}]
+(defn login [{:keys [datomic token] :as component} {{:keys [username password appname next back] :as params} :params}]
   (let [{:keys [access-control-allow-origin]} component
         white-list (-> access-control-allow-origin (clojure.string/split #" "))]
-    (if-not (and (some #(str/starts-with? next %) white-list)
-                 (some #(str/starts-with? back %) white-list))
-      {:status 403 :body "Redirect to specified url is forbidden."}
-      (if-let [user (auth-by-password datomic username password appname)]
-        (let [access-token (token/new-token token user)]
-          (-> (redirect next)
-              (assoc-in [:session :identity] (select-keys user [:user/id :permissions]))
-              (assoc-in [:body] (str access-token))))
-        (redirect (str back "?error=true"))))))
+    (if-not (and (some #(str/starts-with? (str next) %) white-list)
+                 (some #(str/starts-with? (str back) %) white-list))
+      {:status 403 :body (pr-str ["Redirect to specified url is forbidden."])}
+      (if-let [message (:message (validate [false {:edn params}]
+                                           :username [v/required]
+                                           :password [v/required]
+                                           :appname  [v/required]))]
+        (-> (redirect (str back "?error=true"))
+            (assoc-in [:body] (pr-str (:messages message))))
+        (if-let [user (auth-by-password datomic username password appname)]
+          (let [access-token (token/new-token token user)]
+            (-> (redirect next)
+                (assoc-in [:session :identity] (select-keys user [:user/id :permissions]))
+                (assoc-in [:body] (pr-str {:token (str access-token)}))))
+          (-> (redirect (str back "?error=true"))
+              (assoc-in [:body] (pr-str ["Autification failure."]))))))))
 
 (defn logout [{:keys [datomic token]} {{:keys [next]} :params}]
   (-> (redirect next)
@@ -125,13 +125,21 @@
     :allowed-methods [:get :post]
     :malformed? (fn [ctx]
                   (validate (parse-body ctx)
-                            :user/id [v/required [v/matches #"^[\w\-]+$"]]
-                            :user/password [v/required [v/matches #"^[\w\-]+$"]]
-                            :roll [v/required [v/matches #"^[\w\-]+$"]]))
-    :post! (fn [{user :edn :as ctx}]
+                            :user/password [[v/required :pre (comp nil? :user/token)]
+                                            [v/min-count 8 :message "Password must be at least 8 characters long." :pre (comp nil? :user/token)]]
+                            :user/token    [[v/required :pre (comp nil? :user/password)]
+                                            [v/matches #"[0-9a-z]{16}" :pre (comp nil? :user/password)]]
+                            :user/id       [[v/required]
+                                            [v/min-count 3 :message "Username must be at least 3 characters long."]
+                                            [v/max-count 20 :message "Username is too long."]
+                                            [unique-name-validator datomic]]
+                            :roll          [[v/required]
+                                            [v/matches #"^[\w\-]+$"]
+                                            [exist-roll-validator datomic]]))
+    :post! (fn [{user :edn}]
              (let [roll-name (:roll user)
                    user (select-keys user [:user/id :user/password])]
-               (signup-to-default datomic user roll-name)))
+               (signup datomic user roll-name)))
     :handle-ok (fn [_]
                  (d/query datomic
                           '[:find [(pull ?e [:user/id]) ...]
@@ -142,7 +150,7 @@
 
   (start [component]
 
-         ;; Create the initil user and roll.
+         ;; Create an initil user and rolls.
          (->> [{:db/id (d/tempid :db.part/user)
                 :roll/name "admin"
                 :roll/permissions [:permission/read-job
@@ -168,7 +176,7 @@
                               :in $ ?n
                               :where [?e :user/id ?n]]
                             "admin")
-           (signup-to-default datomic {:user/id "admin" :user/password "password123"} "admin"))
+           (signup datomic {:user/id "admin" :user/password "password123"} "admin"))
 
          component)
 
--- test/clj/job_streamer/control_bus/component/auth_test.clj
+++ test/clj/job_streamer/control_bus/component/auth_test.clj
@@ -1,5 +1,6 @@
 (ns job-streamer.control-bus.component.auth-test
   (:require (job-streamer.control-bus.component [auth :as auth]
+                                                [token :as token]
                                                 [apps :as apps]
                                                 [datomic :refer [datomic-component] :as d]
                                                 [migration :refer [migration-component]])
@@ -13,3 +14,188 @@
             [clojure.edn :as edn]
             [clj-time.format :as f]))
 
+(def test-config
+  {:datomic {:recreate? true
+             :uri "datomic:mem://test"}
+   })
+
+(def config
+  (meta-merge config/defaults
+              config/environ
+              test-config))
+
+(defn new-system [config]
+  (-> (component/system-map
+        :apps    (apps/apps-component (:apps config))
+        :auth    (auth/auth-component (:auth config))
+        :token   (token/token-provider-component (:token config))
+        :datomic (datomic-component   (:datomic config))
+        :migration (migration-component {:dbschema model/dbschema}))
+      (component/system-using
+        {:auth [:token :datomic :migration :apps]
+         :apps [:datomic :migration]
+         :migration [:datomic]})
+      (component/start-system)))
+
+(deftest login
+  (let [system (new-system config)
+        handler (partial  auth/login (:auth system))]
+    (testing "login success"
+      (let [request {:request-method :post
+                     :params {:username "admin" :password "password123" :appname "default" :next "http://localhost:3000" :back "http://localhost:3000/login"}}
+            {:keys [status body session headers] :as res} (handler request)]
+        (are [x y] (= x y)
+             302                                        status
+             "http://localhost:3000"                    (get-in headers ["Location"])
+             true                                       (-> body edn/read-string :token empty? not)
+             {:user/id "admin"
+              :permissions #{:permission/update-job
+                             :permission/read-job
+                             :permission/create-job
+                             :permission/delete-job
+                             :permission/execute-job}} (:identity session))))
+    (testing "redirect back url is forbidden"
+      (let [request {:request-method :post
+                     :params {:username "admin" :password "password123" :appname "default" :next "http://localhost:3000" :back "http://forbidden:3000/login"}}
+            {:keys [status body session] :as res} (handler request)]
+        (are [x y] (= x y)
+             403                                         status
+             ["Redirect to specified url is forbidden."] (-> body edn/read-string)
+             nil                                         (:identity session))))
+    (testing "redirect next url is forbidden"
+      (let [request {:request-method :post
+                     :params {:username "admin" :password "password123" :appname "default" :next "http://forbidden:3000" :back "http://localhost:3000/login"}}
+            {:keys [status body session] :as res} (handler request)]
+        (are [x y] (= x y)
+             403                                         status
+             ["Redirect to specified url is forbidden."] (-> body edn/read-string)
+             nil                                         (:identity session))))
+    (testing "login failure, lacking any patameter"
+      (let [request {:request-method :post
+                     :params {:password "password123" :appname "default" :next "http://localhost:3000" :back "http://localhost:3000/login"}}
+            {:keys [status body session headers] :as res} (handler request)]
+        (are [x y] (= x y)
+             302                                      status
+             "http://localhost:3000/login?error=true" (get-in headers ["Location"])
+             ["username must be present"]             (-> body edn/read-string)
+             nil                                      (:identity session))))
+    (testing "login failure, authentification failure"
+      (let [request {:request-method :post
+                     :params {:username "admin" :password "badpassword" :appname "default" :next "http://localhost:3000" :back "http://localhost:3000/login"}}
+            {:keys [status body session headers] :as res} (handler request)]
+        (are [x y] (= x y)
+             302                                      status
+             "http://localhost:3000/login?error=true" (get-in headers ["Location"])
+             ["Autification failure."]                (-> body edn/read-string)
+             nil                                      (:identity session))))
+    (testing "login as created user"
+      (let [request {:request-method :post
+                     :content-type "application/edn"
+                     :body (pr-str {:user/id "addeduser" :user/password "password123" :roll "watcher"})}
+            {:keys [status body]} (let [handler (auth/list-resource (:auth system))] (handler request))]
+        (is (= 201 status)))
+      (let [request {:request-method :post
+                     :params {:username "addeduser" :password "password123" :appname "default" :next "http://localhost:3000" :back "http://localhost:3000/login"}}
+            {:keys [status body session headers] :as res} (handler request)]
+        (are [x y] (= x y)
+             {:user/id                              "addeduser"
+              :permissions #{:permission/read-job}} (:identity session))))))
+
+(deftest logout
+  (let [system (new-system config)
+        handler (partial auth/logout (:auth system))]
+    (testing "logout success"
+      (let [request {:request-method :get
+                     :identity {:user/id "admin"}
+                     :params {:next "http://localhost:3000/login"}}
+            {:keys [status session headers] :as res} (handler request)]
+        (are [x y] (= x y)
+             302                           status
+             "http://localhost:3000/login" (get-in headers ["Location"])
+             nil                           (:identity session))))))
+
+(deftest list-resource
+  (let [system (new-system config)
+        handler (auth/list-resource (:auth system))]
+    (testing "get only default user"
+      (let [request {:request-method :get}
+            {:keys [status body]} (handler request)]
+        (are [x y] (= x y)
+             200     status
+             1       (-> body edn/read-string count)
+             "admin" (-> body edn/read-string first :user/id))))
+    (testing "lacking id"
+      (let [request {:request-method :post
+                     :content-type "application/edn"
+                     :body (pr-str {:user/password "password123" :roll "watcher"})}
+            {:keys [status body]} (handler request)]
+        (are [x y] (= x y)
+             400 status
+             ["id must be present"] (-> body edn/read-string :messages))))
+    (testing "lacking password"
+      (let [request {:request-method :post
+                     :content-type "application/edn"
+                     :body (pr-str {:user/id "test" :roll "watcher"})}
+            {:keys [status body]} (handler request)]
+        (are [x y] (= x y)
+             400 status
+             ["password must be present" "token must be present"] (-> body edn/read-string :messages))))
+    (testing "lacking roll"
+      (let [request {:request-method :post
+                     :content-type "application/edn"
+                     :body (pr-str {:user/id "test" :user/password "password123"})}
+            {:keys [status body]} (handler request)]
+        (are [x y] (= x y)
+             400 status
+             ["roll must be present"] (-> body edn/read-string :messages))))
+    (testing "id does not satisfy min length"
+      (let [request {:request-method :post
+                     :content-type "application/edn"
+                     :body (pr-str {:user/id "te" :user/password "password123" :roll "watcher"})}
+            {:keys [status body]} (handler request)]
+        (are [x y] (= x y)
+             400 status
+             ["Username must be at least 3 characters long."] (-> body edn/read-string :messages))))
+    (testing "id does not satisfy max length"
+      (let [request {:request-method :post
+                     :content-type "application/edn"
+                     :body (pr-str {:user/id "test12345678901234567" :user/password "password123" :roll "watcher"})}
+            {:keys [status body]} (handler request)]
+        (are [x y] (= x y)
+             400 status
+             ["Username is too long."] (-> body edn/read-string :messages))))
+    (testing "password does not satisfy min length"
+      (let [request {:request-method :post
+                     :content-type "application/edn"
+                     :body (pr-str {:user/id "test" :user/password "passwor" :roll "watcher"})}
+            {:keys [status body]} (handler request)]
+        (are [x y] (= x y)
+             400 status
+             ["Password must be at least 8 characters long."] (-> body edn/read-string :messages))))
+    (testing "id conflicts"
+      (let [request {:request-method :post
+                     :content-type "application/edn"
+                     :body (pr-str {:user/id "admin" :user/password "password123" :roll "watcher"})}
+            {:keys [status body]} (handler request)]
+        (are [x y] (= x y)
+             400 status
+             ["id is used by someone."] (-> body edn/read-string :messages))))
+    (testing "invalid roll"
+      (let [request {:request-method :post
+                     :content-type "application/edn"
+                     :body (pr-str {:user/id "test" :user/password "password123" :roll "nothing"})}
+            {:keys [status body]} (handler request)]
+        (are [x y] (= x y)
+             400 status
+             ["roll is used by someone."] (-> body edn/read-string :messages))))
+    (testing "create user"
+      (let [request {:request-method :post
+                     :content-type "application/edn"
+                     :body (pr-str {:user/id "test" :user/password "password123" :roll "watcher"})}
+            {:keys [status body]} (handler request)]
+        (is (= 201 status)))
+      (let [request {:request-method :get}
+            {:keys [status body]} (handler request)]
+        (are [x y] (= x y)
+             200     status
+             2       (-> body edn/read-string count))))))
