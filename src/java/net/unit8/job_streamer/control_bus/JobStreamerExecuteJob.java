d4dad21033f8bf8e62ef7765832ddafcaf59d352
Kazuki Tsutsumi <rixi223.kazuki@gmail.com>
2017-02-09 14:57:53 +0900

    Change Job classes to call clj code directly.

--- src/java/net/unit8/job_streamer/control_bus/JobStreamerExecuteJob.java
+++ src/java/net/unit8/job_streamer/control_bus/JobStreamerExecuteJob.java
@@ -1,14 +1,15 @@
 package net.unit8.job_streamer.control_bus;
 
+import clojure.java.api.Clojure;
+import clojure.lang.*;
 import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
-import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
+
+import net.unit8.job_streamer.control_bus.util.SystemUtil;
 
 /**
  * @author kawasima
@@ -19,31 +20,22 @@ public class JobStreamerExecuteJob implements Job {
     public void execute(JobExecutionContext context) throws JobExecutionException {
         JobDataMap data = context.getMergedJobDataMap();
         String jobName = data.getString("job-name");
-        String host = data.getString("host");
-        long port = data.getLong("port");
         String appName = data.getString("app-name");
-        String token = data.getString("token");
 
-        URLConnection conn;
-        try {
-            URL url = new URL("http://" + host + ":" + port
-                    + "/" + appName
-                    + "/job/" + jobName
-                    + "/executions");
+        Object system = SystemUtil.getSystem();
+        Object jobs = RT.get(system, Keyword.intern("jobs"));
 
-            conn = url.openConnection();
-            conn.setReadTimeout(5000);
-            conn.setConnectTimeout(1000);
-            ((HttpURLConnection) conn).setRequestMethod("POST");
-            ((HttpURLConnection) conn).setRequestProperty("Authorization", "Token " + token);
-            conn.connect();
-            int statusCode = ((HttpURLConnection) conn).getResponseCode();
-            if (statusCode != HttpURLConnection.HTTP_CREATED) {
-                throw new JobExecutionException("HttpRequest not success:" + statusCode);
-            }
-        } catch (IOException ex) {
-            throw new JobExecutionException(ex);
+        IFn executionsResource = Clojure.var("job-streamer.control-bus.component.jobs", "executions-resource");
+        IFn handler = (IFn) executionsResource.invoke(jobs, appName, jobName);
+        PersistentHashMap request = PersistentHashMap.create(
+                Keyword.intern("request-method"), Keyword.intern("post"),
+                Keyword.intern("identity"), PersistentHashMap.create(
+                    Keyword.intern("permissions"), PersistentHashSet.create(
+                        Keyword.intern("permission", "execute-job"))),
+                Keyword.intern("content-type"), "application/edn");
+        long statusCode = (long) RT.get(handler.invoke(request), Keyword.intern("status"));
+        if (statusCode != (long) HttpURLConnection.HTTP_CREATED) {
+            throw new JobExecutionException("HttpRequest not success:" + statusCode);
         }
-
     }
 }
