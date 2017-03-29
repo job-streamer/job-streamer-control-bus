3d1fc10ba3146f6eab8d257cb44dccec31d5ae79
seki <schokolade0317@gmail.com>
2017-03-29 19:23:39 +0900

    insert null check

--- src/java/net/unit8/job_streamer/control_bus/JobStreamerExecuteJob.java
+++ src/java/net/unit8/job_streamer/control_bus/JobStreamerExecuteJob.java
@@ -24,6 +24,7 @@ public class JobStreamerExecuteJob implements Job {
         JobDataMap data = context.getMergedJobDataMap();
         String jobName = data.getString("job-name");
         String appName = data.getString("app-name");
+        if (jobName == null || appName == null) return;
         logger.info("execute job that " + "jobName:" + jobName + " appName:" + appName + " execution-id:" + data.getString("execution-id") + " command:" + data.getString("command"));
 
         Object system = SystemUtil.getSystem();
